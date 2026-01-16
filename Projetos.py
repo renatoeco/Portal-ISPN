cc8c7a4aeaeaedc48ef33901a64dd782e9485ba0
renatoeco <renato.eco@gmail.com>
2026-01-16 11:26:02 -0300

    Remoção de comentários.

--- Projetos.py
+++ Projetos.py
@@ -14,21 +14,6 @@ st.set_page_config(layout="wide")
 st.logo("images/logo_ISPN_horizontal_ass.png", size='large')
 
 
-# ######################################################################################################
-# # CSS PARA DIALOGO MAIOR
-# ######################################################################################################
-# st.markdown(
-#     """
-# <style>
-# div[data-testid="stDialog"] div[role="dialog"]:has(.big-dialog) {
-#     width: 50vw;
-    
-# }
-# </style>
-# """,
-#     unsafe_allow_html=True,
-# )
-
 
 ######################################################################################################
 # FUNÇÕES AUXILIARES
@@ -1392,84 +1377,6 @@ with tab1:
 
     df_original = df_projetos_ispn_filtrado_show.copy()
     
-    # Exibindo o DataFrame
-
-    # ----------------------------------------------------
-    # MODO EDIÇÃO
-    # ----------------------------------------------------
-
-    # if "modo_edicao" not in st.session_state:
-    #     st.session_state.modo_edicao_1 = False
-
-    # if set(st.session_state.tipo_usuario) & {"admin", "coordenador(a)"}:
-    #     col1, col2 = st.columns([4, 1])
-    #     col1.toggle("Modo de edição", value=False, key="modo_edicao")
-        
-
-    #     if st.session_state.modo_edicao:
-    #         df_editado = st.data_editor(
-    #             df_projetos_ispn_filtrado_show,
-    #             use_container_width=True,
-    #             hide_index=True,
-    #             column_config={
-    #                 "Código": st.column_config.TextColumn(),
-    #                 "Valor": st.column_config.TextColumn(),
-    #                 "Programa": st.column_config.TextColumn(disabled=True),
-    #                 "Doador": st.column_config.TextColumn(disabled=True),
-    #                 "Situação": st.column_config.SelectboxColumn(
-    #                     options=["Em andamento", "Finalizado", "Pausado"]
-    #                 ),
-    #                 "Início do contrato": st.column_config.TextColumn(),
-    #                 "Fim do contrato": st.column_config.TextColumn(),
-    #             },
-    #             key="editor_projetos_ispn"
-    #         )
-
-    #         container_botao = st.container(horizontal=True, horizontal_alignment='left')
-
-    #         st.write("")
-
-    #         if container_botao.button("Salvar alterações"):
-
-    #             MAPA_CAMPOS_EDITOR_PARA_MONGO = {
-    #                 "Nome do projeto": "nome_do_projeto",
-    #                 "Situação": "status",
-    #                 "Início do contrato": "data_inicio_contrato",
-    #                 "Fim do contrato": "data_fim_contrato",
-    #                 "Código": "codigo",
-    #                 "Valor": "valor"
-    #             }
-
-    #             for idx, row in df_editado.iterrows():
-
-    #                 codigo = row["Código"]
-    #                 if not codigo:
-    #                     continue
-
-    #                 update_fields = {}
-
-    #                 for col_tela, campo_mongo in MAPA_CAMPOS_EDITOR_PARA_MONGO.items():
-
-    #                     novo = row[col_tela]
-    #                     antigo = df_original.loc[idx, col_tela]
-
-    #                     if novo != antigo:
-    #                         update_fields[campo_mongo] = normalizar_valor(campo_mongo, novo)
-
-    #                 if update_fields:
-    #                     projetos_ispn.update_one(
-    #                         {"codigo": codigo},
-    #                         {"$set": update_fields}
-    #                     )
-
-    #             st.success("Alterações salvas com sucesso!")
-    #             time.sleep(2)
-    #             st.rerun()
-
-    #     else:
-    #         ajustar_altura_dataframe(df_projetos_ispn_filtrado_show, 1)
-
-    # else:
     ajustar_altura_dataframe(df_projetos_ispn_filtrado_show, 1)
 
 
@@ -2001,11 +1908,6 @@ with tab2:
 
             ui.table(data=df_resumo.drop(columns=["Valor_num"], errors="ignore"))
 
-            # ajustar_altura_dataframe(df_resumo.drop(columns=["Valor_num"], errors="ignore"), 
-            #                             linhas_adicionais=1,
-            #                             hide_index=True, 
-            #                             use_container_width=True
-            #                             )            
 
         # Ao acionar o toggle, mostra todos os lançamentos detalhados
         else:
